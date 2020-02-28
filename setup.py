524cb833ebb2175f079a5394755573d677dcd735
jake <jake@genevatech.net>
2020-02-28 11:14:53 -0700

    Added long description

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='python_tools',
     version='1.0.0',
     description='Link serial ports and tcp sockets together',
+    long_description=README,
+    long_description_content_type="text/markdown",
     url='https://github.com/JakeHillHub/port-linker',
     author='Jake Hill',
     author_email='jakehillgithub@gmail.net',
